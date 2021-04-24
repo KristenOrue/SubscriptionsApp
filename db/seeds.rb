3ba0c32a38e0d0e1874961acf8e7328b6ebc466a
KristenOrue <oruek@oregonstate.edu>
2021-04-23 20:12:49 -0700

    Add comments references the command to start with a clean slate of Plan models

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # Movie.create(title: "The Matrix", video_url: "secret")
-Plan.destroy_all
+# If Duplicates are created when running rails db:seed:
+# Plan.destroy_all
 
 Plan.create(name: "Small", stripe_id: "prod_JMKHhNIfayyvKX", amount: 25_00, interval: "month")
 Plan.create(name: "Small", stripe_id: "prod_JMKIyF65hPCrpv", amount: 250_00, interval: "year")
